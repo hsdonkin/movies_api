67279e9d61c47f57491d0d721013d686f11c6c49
Student <example@example.com>
2019-10-09 14:25:38 -0700

    retool verification from headers to params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 
   def login
-    json_response({:message => "Please Log In! Via Post Request to /login", :url => "http://localhost:3000/login", :username => "", :password => ""})
+    json_response({:message => "Please log in via post request to /login, or register at /register. Enter username and password into params.", :url => "https://cryptic-ridge-59014.herokuapp.com//login", :register => "https://cryptic-ridge-59014.herokuapp.com/register", :username => "", :password => ""})
   end
 
   def create
-    if request.headers["HTTP_USERNAME"] && request.headers["HTTP_PASSWORD"]
-        authenticate_result = User.authenticate(request.headers["HTTP_USERNAME"], request.headers["HTTP_PASSWORD"])
+    if params[:username] && params[:password]
+        authenticate_result = User.authenticate(params[:username], params[:password])
         json_response(authenticate_result)
     end
   end
